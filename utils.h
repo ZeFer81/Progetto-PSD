3873540a8f4d38e4cf70e101774fc150a02808b6
Chhlat Zagli <chhlatzagli02@gmail.com>
2025-05-30 20:54:29 +0200

    funzione swap

--- utils.h
+++ utils.h
@@ -1,3 +1,23 @@
 #include "item-attivita_di_studio.h"
 
+/**
+* swap - Scambia il contenuto di due elementi di tipo Item.
+*
+* Parametri:
+*   a: Puntatore al primo Item da scambiare. Deve essere non-NULL e puntare
+*       a una variabile di tipo Item valida e inizializzata.
+*   b: Puntatore al secondo Item da scambiare. Deve essere non-NULL e puntare
+*       a una variabile di tipo Item valida e inizializzata.
+*
+* Precondizioni:
+*   - `a` != NULL
+*   - `b` != NULL
+*   - `*a` e `*b` devono essere stati allocati e inizializzati correttamente.
+*
+* Postcondizioni:
+*   - Il valore originale di `*a` è ora in `*b`.
+*   - Il valore originale di `*b` è ora in `*a`.
+*   - Nessun altro oggetto o variabile globale viene modificato.
+*   - Non ci sono perdite di memoria né side-effects non dichiarati
+*/
 void swap(Item *a, Item *b);
